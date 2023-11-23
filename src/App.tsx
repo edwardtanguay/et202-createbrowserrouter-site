6d4ce0aa2ed699c6df9062674f26c3d8e08c4629
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-23 10:18:08 +0100

    pages show

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
 	return (
 		<>
 			<h1 className="text-2xl mb-4">Data-Loading Site</h1>
-      <p>skdfjdf</p>
+			<Outlet />
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,27 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { PageWelcome } from "./pages/PageWelcome.tsx";
+import { PageAbout } from "./pages/PageAbout.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/welcome",
+				element: <PageWelcome />,
+			},
+			{
+				path: "/about",
+				element: <PageAbout />,
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<RouterProvider router={router} />
+);
