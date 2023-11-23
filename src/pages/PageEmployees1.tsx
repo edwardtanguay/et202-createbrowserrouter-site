5a9a974112ce60db09ba9e28b8ca8d4610785add
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-23 15:37:48 +0100

    please wait in

--- src/pages/PageEmployees1.tsx
+++ src/pages/PageEmployees1.tsx
@@ -30,7 +30,11 @@ export const PageEmployees1 = () => {
 					})}
 				</>
 			) : (
-				<ImSpinner7 className="text-6xl ml-3 mt-3 animate-spin opacity-50"/>
+				<p>
+				<p className="flex gap-3 items-center mb-4">
+					<ImSpinner7 className="text-6xl ml-3 mt-3 animate-spin opacity-50" /><p className="text-3xl opacity-50">Please wait...</p> 
+				</p>
+				</p>
 			)}
 		</>
 	);
